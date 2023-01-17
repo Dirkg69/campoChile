d3af85bd7b33a68b3f886d8abaa00cd3975950a4
Dirk Goettling <dirkg69@icloud.com>
2023-01-17 19:30:55 -0300

    originAgentCluster set to false

--- app.js
+++ app.js
@@ -77,6 +77,7 @@ app.use(session(sessionConfig));
 app.use(flash());
 app.use(
 	helmet({
+		originaAgentCluster: false,
 		crossOriginOpenerPolicy: false,
 		crossOriginEmbedderPolicy: false,
 		crossOriginResourcePolicy: false,
