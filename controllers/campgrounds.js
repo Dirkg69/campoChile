cbcb4449073a247c2d8ad2f5ca3261caf6754872
Dirk Goettling <dirkg69@icloud.com>
2023-01-30 14:46:54 -0300

    design changes

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -27,8 +27,6 @@ module.exports.createCampground = async (req, res) => {
 		res.redirect(`/campgrounds/${campground._id}`);
 };
 
-
-
 module.exports.showCampground = async (req, res) => {
 	const campground = await Campground.findById(req.params.id)
 		.populate({
