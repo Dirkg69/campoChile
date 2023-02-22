7cfba3098dd12cdd0eb4ac353759e41972912d41
Dirk Goettling <dirkg69@icloud.com>
2023-02-22 01:05:36 -0300

    background color grey

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -4,9 +4,10 @@ const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require('../cloudinary');
 
+
 module.exports.index = async (_req, res) => {
 	const campgrounds = await Campground.find({});
-	res.render('campgrounds/index', { campgrounds });
+	res.render('campgrounds/index', {campgrounds} );
 };
 
 module.exports.renderNewForm = (_req, res) => {
