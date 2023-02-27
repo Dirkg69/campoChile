85ce8562984502fa07a60f2ea722adc796664288
Dirk Goettling <dirkg69@icloud.com>
2023-02-27 15:10:02 -0300

    set node_env to production

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({ replaceWith: '_' }));
 
 const secret = process.env.SECRET;
-const store = new MongoDBStore.create({
+const store = MongoDBStore.create({
 	mongoUrl: dbUrl,
 	secret,
 	touchAfter: 24 * 60 * 60,
